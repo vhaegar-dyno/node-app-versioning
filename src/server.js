5fb0c7901afcdc639295a8b008f1c3b248bed778
rahul-holmium <rahul.holmiumtechnologies>
2025-05-02 18:17:22 +0530

    :big: fix(application): removed v1 from api endpoints

--- src/server.js
+++ src/server.js
@@ -5,6 +5,6 @@ const { dbConnect } = require('./config/db');
 
 app.listen(port, whitelistIP, () => {
 	logger.info(`🖥️ 🚀  Server is running on process ${process.pid} at port ${port}`);
-	logger.info(`Server URL: ${serverUrl}/api/v1`);
+	logger.info(`Server URL: ${serverUrl}/api`);
 	dbConnect();
 });
