7426dcbcd84d04819484af0edbf3265cfbd2b20e
rahul-holmium <rahul.holmiumtechnologies>
2025-05-02 20:14:01 +0530

    :sparkles: feat(application): v2 introduces

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,15 @@ const routes = (app) => {
 	app.use(ENDPOINT + '/health', healthCheckRoutes);
 	app.use(ENDPOINT + '/user', userRoutes);
 	app.use(ENDPOINT + '/auth', auth, authRoutes);
+	app.use(ENDPOINT + '/v2', (req, res) => {
+		try {
+			return res.status(200).json({ status: true, message: 'Version: v2.0.0 is running' });
+		} catch (err) {
+			return res
+				.status(500)
+				.json({ status: false, message: err.message || 'Something went wrong' });
+		}
+	});
 };
 
 module.exports = routes;
