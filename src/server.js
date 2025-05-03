b1b6d19d0ada42928970e7ce7b242e803ddb53c3
rahul-holmium <rahul.holmiumtechnologies>
2025-05-03 12:22:38 +0530

    :big: fix(application): stopped db connection for now

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const { port, whitelistIP, serverUrl } = require('./config/config');
 const app = require('./app');
 const logger = require('./config/logger');
-const { dbConnect } = require('./config/db');
+// const { dbConnect } = require('./config/db');
 
 app.listen(port, whitelistIP, () => {
 	logger.info(`🖥️ 🚀  Server is running on process ${process.pid} at port ${port}`);
 	logger.info(`Server URL: ${serverUrl}/api`);
-	dbConnect();
+	// dbConnect();
 });
